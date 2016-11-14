70ecd1aa729ccb83f4404c1ce86f108c2a047181
Sanjay Nambiar <sanjay.mnambiar@gmail.com>
2016-11-13 20:52:47 -0500

    Development (#3)
    * Adding cookie check for bypassing GA
    
    * Added about page
    
    * Added Resume

--- styles/index.js
+++ styles/index.js
@@ -1,20 +1,35 @@
 var hide_lists = function(cb) {
+    $('#about').fadeOut(300);
     $('#posts').fadeOut(300);
     $('#projects').fadeOut(300);
+    $('#about-btn').removeClass('disabled')
     $('#posts-btn').removeClass('disabled');
     $('#projects-btn').removeClass('disabled')
 };
-var show_projects = function() {
+var show_projects = function() {    
     $('#posts-btn').removeClass('disabled');
+    $('#about-btn').removeClass('disabled');
+    $('#about').fadeOut(300);
     $('#posts').fadeOut(300, function() {
-        $('#projects').fadeIn(300)
+        $('#projects').fadeIn(300);
     });
     $('#projects-btn').addClass('disabled')
 };
 var show_posts = function() {
     $('#projects-btn').removeClass('disabled');
-    $('#projects').fadeOut(function() {
-        $('#posts').fadeIn(300)
+    $('#about-btn').removeClass('disabled');
+    $('#about').fadeOut(300);
+    $('#projects').fadeOut(300, function() {
+        $('#posts').fadeIn(300);
     });
     $('#posts-btn').addClass('disabled')
+};
+var show_about = function() {
+    $('#posts-btn').removeClass('disabled');
+    $('#posts-btn').removeClass('disabled');
+    $('#projects').fadeOut(300);
+    $('#posts').fadeOut(300, function() {
+        $('#about').fadeIn(300);
+    });
+    $('#about-btn').addClass('disabled')
 };
